7714c132abcdadf436e45a963d2f6d0eed95d653
kSurjeet <83670326+kSurjeet@users.noreply.github.com>
2024-08-12 14:27:28 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,9 +34,16 @@ const writeTasksToFile = (tasks) => {
 };
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello To Do");
-});
+router.get("/", (req, res) => {
+  res.send("hello World!!!');
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+ });
+
+
 //to get to do list
 app.get('/todos', (req, res) => {
     const todolist = readTasksFromFile();
